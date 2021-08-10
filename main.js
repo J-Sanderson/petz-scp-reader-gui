b357f2087061c000477f6915b6910854df632b8c
JSanderson <ju.a.sanderson@gmail.com>
2021-08-10 16:11:50 +0100

    feat: add remaining parsers

--- main.js
+++ main.js
@@ -44,9 +44,56 @@ ipcMain.on('ondialogopen', (event) => {
                 .uint32('animations')
                 .uint32('unknown')
                 .skip(8)
-                .uint32('actions');
+                .uint32('actions')
             const header = headerParser.parse(data)
             console.log(header)
+
+            const individualActionParser = new Parser()
+                .endianess('little')
+                .uint32('actionID')
+                .uint32('numScripts')
+                .uint32('animation1')
+                .uint32('animation2')
+                .skip(4)
+                .uint16('unknown')
+                .uint16('sameAnimationModifier')
+                .skip(4)
+                .uint32('script')
+            const actionParser = new Parser()
+                .endianess('little')
+                .skip(82) //skip header
+                .array('actions', {
+                    type: individualActionParser,
+                    length: header.actions
+                })
+            const actions = actionParser.parse(data)
+            console.log(actions)
+
+            const scriptDwordParser = new Parser()
+                .endianess('little')
+                .skip(82 + (header.actions * 32)) //skip header and actions
+                .uint32('dwordCount')
+            const scriptDwords = scriptDwordParser.parse(data)
+            console.log(scriptDwords)
+
+            const dwordParser = new Parser()
+                .endianess('little')
+                .array('bytes', {
+                    type: "uint8",
+                    length: 4,
+                });
+            const scriptParser = new Parser()
+                .endianess('little')
+                .skip(82 + (header.actions * 32) + 4) //skip header, actions, dword count
+                .array('dwords', {
+                    type: dwordParser,
+                    length: scriptDwords.dwordCount,
+                })
+                .string('copyright', {
+                    zeroTerminated: true,
+                });
+            const script = scriptParser.parse(data);
+            console.log(script)
         })
     })
 })
